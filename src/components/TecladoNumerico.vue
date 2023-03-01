4f3ed8db46e15448e526b9216c6cbd6f8135e8f2
Luis Cabrera Benito <parzibyte@gmail.com>
2023-02-28 22:42:47 -0600

    Teclado numérico para mayor velocidad

---
+++ src/components/TecladoNumerico.vue
@@ -0,0 +1,38 @@
+<template>
+  <div style="max-width: 300px" class="has-text-centered">
+    <b-button
+      size="is-large"
+      @click="teclaPresionada(tecla)"
+      type="is-primary"
+      class="m-2"
+      v-for="tecla in teclado"
+      :key="tecla"
+      >{{ tecla }}</b-button
+    >
+    <b-button size="is-large" @click="borrar" class="m-2" type="is-warning">
+      <b-icon icon="backspace"></b-icon
+    ></b-button>
+    <b-button size="is-large" @click="borrarTodo" class="m-2" type="is-danger"
+      >C</b-button
+    >
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    teclado: "1234567890".split(""),
+  }),
+  methods: {
+    borrarTodo() {
+      this.$emit("borrarTodo");
+    },
+    teclaPresionada(tecla) {
+      this.$emit("teclaPresionada", tecla);
+    },
+    borrar() {
+      this.$emit("borrar");
+    },
+  }
+}
+
+</script>
