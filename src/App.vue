ab404e5f6de214ae3575a8bf5a756d4d50b910cf
Luis Cabrera Benito <parzibyte@gmail.com>
2023-02-28 22:42:21 -0600

    Creando base de datos

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,33 @@
 <template>
-    <div class="container">
-      <div class="columns">
-        <div class="column">
-          <section>
-            <h1 class="is-size-1">Probando Buefy</h1>
-            <button class="button is-medium" @click="mostrarToast">
-                Mostrar un toast
-            </button>
-          </section>
-        </div>
-      </div>
-    </div>
+  <div>
+    <b-navbar type="is-primary">
+      <template #start>
+        <b-navbar-item tag="router-link" :to="{ name: 'VerRutas' }"
+          ><b-icon icon="routes" class="mr-1"></b-icon> Rutas</b-navbar-item
+        >
+        <b-navbar-item tag="router-link" :to="{ name: 'GestionarUnidades' }"
+          ><b-icon icon="bus-multiple" class="mr-1"></b-icon>
+          Unidades</b-navbar-item
+        >
+        <b-navbar-item tag="router-link" :to="{ name: 'VerRoles' }"
+          ><b-icon icon="sync" class="mr-1"></b-icon> Roles</b-navbar-item
+        >
+      </template>
+    </b-navbar>
+    <section class="section">
+      <router-view />
+    </section>
+  </div>
 </template>
 
 <script>
+import { iniciarBaseDeDatos } from "./services/BaseDeDatosService"
 export default {
   name: 'app',
-  methods: {
-    mostrarToast(){
-      this.$buefy.toast.open("¡Hola, Buefy! Parzibyte.me")
-    }
-  }
+  async beforeCreate() {
+    const ok = await iniciarBaseDeDatos();
+    console.log({ ok });
+  },
+
 }
 </script>
---
+++ src/services/BaseDeDatosService.js
@@ -0,0 +1,95 @@
+import workerInjector from "jsstore/dist/worker_injector";
+
+import { Connection } from "jsstore/";
+import { DATA_TYPE } from "jsstore";
+
+const obtenerBaseDeDatos = () => {
+    const tablaUnidades = {
+        name: "unidades",
+        columns: {
+            id: {
+                primaryKey: true,
+                autoIncrement: true
+            },
+            numero: {
+                notNull: true,
+                dataType: DATA_TYPE.Number,
+            },
+            entrada: {
+                dataType: DATA_TYPE.Number,
+                notNull: true,
+            },
+            salida: {
+                dataType: DATA_TYPE.Number,
+                default: 0,
+            },
+            segundosDesdeSalidaAnterior: {
+                notNull: true,
+                dataType: DATA_TYPE.Number,
+                default: 0,
+            },
+            ruta: {
+                dataType: DATA_TYPE.Object,
+                notNull: true
+            }
+        }
+    };
+
+    const tablaRutas = {
+        name: "rutas",
+        columns: {
+            id: {
+                primaryKey: true,
+                autoIncrement: true
+            },
+            nombre: {
+                notNull: true,
+                dataType: DATA_TYPE.String,
+            },
+        }
+    };
+
+    const tablaRoles = {
+        name: "roles",
+        columns: {
+            id: {
+                primaryKey: true,
+                autoIncrement: true
+            },
+            nombre: {
+                notNull: true,
+                dataType: DATA_TYPE.String,
+            },
+            rutas: {
+                notNull: true,
+                dataType: DATA_TYPE.Array,
+            }
+        }
+    };
+    const tablaAjustes = {
+        name: "ajustes",
+        columns: {
+            id: {
+                primaryKey: true,
+                autoIncrement: true
+            },
+            rolSeleccionado: {
+                notNull: true,
+                dataType: DATA_TYPE.Object,
+            },
+        }
+    };
+    const baseDeDatos = {
+        name: "checador",
+        tables: [tablaUnidades, tablaRutas, tablaRoles, tablaAjustes],
+    };
+    return baseDeDatos;
+};
+
+const conexion = new Connection();
+conexion.addPlugin(workerInjector);
+export default conexion;
+export const iniciarBaseDeDatos = async () => {
+    const baseDeDatos = obtenerBaseDeDatos();
+    return await conexion.initDb(baseDeDatos);
+};
