6881c313ff1534c6026a29f1b8da0abf6fa0ecff
Luis Cabrera Benito <parzibyte@gmail.com>
2023-03-02 10:11:19 -0600

    No crear maps y arreglar publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // En realidad debería ser "./" pero al compilar se remueve el ./ así que mejor se pone doble para que solo
+  // se remueva el primero. Gracias a: https://github.com/vuejs/vue-cli/issues/1623#issuecomment-729086789
+  publicPath: process.env.NODE_ENV === 'production' ? '././' : './',
+  productionSourceMap: false, // Deshabilitar generación de archivos map
 })
