3f02f8989b697967adce1e2852ec316af95eb816
Luis Cabrera Benito <parzibyte@gmail.com>
2023-03-10 21:34:07 -0600

    Reporte de unidades

---
+++ src/components/Unidades/ReporteUnidades.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <p :style="{backgroundColor:unidad.ruta.color}" v-for="unidad in unidades" :key="unidad.id">
+        {{ unidad.numero }} {{unidad.ruta.nombre}} {{unidad.salida | fechaAPartirDeMilisegundos}} 
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+import { obtenerTodasLasUnidadesParaReporte } from "@/services/UnidadesService";
+export default {
+  data: () => ({
+    unidades: [],
+  }),
+  async mounted() {
+    await this.obtenerUnidades();
+  },
+  methods: {
+    async obtenerUnidades() {
+      this.unidades = await obtenerTodasLasUnidadesParaReporte();
+    },
+  },
+};
+</script>
